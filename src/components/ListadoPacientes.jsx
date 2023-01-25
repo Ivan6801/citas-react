2fad167f2521598bb220f1f03c8547a65ebba02b
Ivan Gonzalez <hal308366@gmail.com>
2023-01-25 13:16:09 -0800

    Mostrar textos de forma condicial

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,17 +1,41 @@
 import React from "react";
 import { Paciente } from "./Paciente";
 
-export function ListadoPacientes() {
+export function ListadoPacientes({ pacientes }) {
+  console.log(pacientes.length === 0);
+
   return (
-    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-      <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
-      <p className="text-xl mt-5 mb-10 text-center">
-        Administra tus {""}
-        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
-      </p>
-      <Paciente />
-      <Paciente />
-      <Paciente />
-    </div>
+    <>
+      <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+        {pacientes && pacientes.length ? (
+          <>
+            <h2 className="font-black text-3xl text-center">
+              Listado Pacientes
+            </h2>
+            <p className="text-xl mt-5 mb-10 text-center">
+              Administra tus {""}
+              <span className="text-indigo-600 font-bold">
+                Pacientes y Citas
+              </span>
+            </p>
+            {pacientes.map((paciente) => {
+              return <Paciente key={paciente.id} paciente={paciente} />;
+            })}
+          </>
+        ) : (
+          <>
+            <h2 className="font-black text-3xl text-center">
+              No Hay Pacientes
+            </h2>
+            <p className="text-xl mt-5 mb-10 text-center">
+              Comienza agregando pacientes
+              <span className="text-indigo-600 font-bold">
+                Y aparecerán en estre lugar
+              </span>
+            </p>
+          </>
+        )}
+      </div>
+    </>
   );
 }
